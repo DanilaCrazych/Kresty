1f4f729710fb32d3fe4280937f067a2c20b3d3b5
Данил <mag7istr@mail.ru>
2023-10-05 08:32:45 +0300

    It`s Work

--- src/main/java/com/example/calc/HelloController.java
+++ src/main/java/com/example/calc/HelloController.java
@@ -4,14 +4,19 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class HelloController {
+
+    int[] board = new int[]{
+            0, 0, 0,
+            0, 0, 0,
+            0, 0, 0
+    };
+
     @FXML
     private Label welcomeText;
     @FXML
-    private Label turn;
+    private Label turn, win;
 
     @FXML
     private Button Lverh, Verh, Rverh, Lmid, Rmid, Ldown, Down, Rdown;
@@ -19,19 +24,68 @@ public class HelloController {
     @FXML
     private Button centre;
 
+
+
+
     public void initialize() {
         turn.setText("x");
     }
+
+
+    @FXML
+    protected void checkWinner() {
+        if (board[0] == 1 && board[1] == 1 && board[2] == 1) {
+            win.setText("X win");
+        } else if (board[3] == 1 && board[4] == 1 && board[5] == 1) {
+            win.setText("X win");
+        } else if (board[6] == 1 && board[7] == 1 && board[8] == 1) {
+            win.setText("X win");
+        } else if (board[0] == 1 && board[4] == 1 && board[8] == 1) {
+            win.setText("X win");
+        } else if (board[2] == 1 && board[4] == 1 && board[6] == 1) {
+            win.setText("X win");
+        } else if (board[0] == 1 && board[3] == 1 && board[6] == 1) {
+            win.setText("X win");
+        } else if (board[1] == 1 && board[4] == 1 && board[7] == 1) {
+            win.setText("X win");
+        } else if (board[2] == 1 && board[5] == 1 && board[8] == 1) {
+            win.setText("X win");
+        }
+        else if (board[0] == 2 && board[1] == 2 && board[2] == 2) {
+            win.setText("O win");
+        } else if (board[3] == 2 && board[4] == 2 && board[5] == 2) {
+            win.setText("O win");
+        } else if (board[6] == 2 && board[7] == 2 && board[8] == 2) {
+            win.setText("O win");
+        } else if (board[0] == 2 && board[4] == 2 && board[8] == 2) {
+            win.setText("O win");
+        } else if (board[2] == 2 && board[4] == 2 && board[6] == 2) {
+            win.setText("O win");
+        } else if (board[0] == 2 && board[3] == 2 && board[6] == 2) {
+            win.setText("O win");
+        } else if (board[1] == 2 && board[4] == 2 && board[7] == 2) {
+            win.setText("O win");
+        } else if (board[2] == 2 && board[5] == 2 && board[8] == 2) {
+            win.setText("O win");
+        }
+        else if(board[0] != 0 && board[1] != 0 && board[2] != 0 && board[3] != 0 && board[4] != 0 && board[5] != 0 && board[6] != 0 && board[7] != 0 && board[8] != 0) {
+            win.setText("Draw");}
+    }
+
 
     @FXML
     protected void playLverh() {
         if (turn.getText().equals("x")) {
             Lverh.setText("х");
             turn.setText("o");
+            board[0] = 1;
         } else {
             Lverh.setText("o");
             turn.setText("x");
+            board[0] =2;
         }
+        Lverh.setDisable(true);
+        checkWinner();
     }
 
     @FXML
@@ -39,10 +93,14 @@ public class HelloController {
         if (turn.getText().equals("x")) {
             Verh.setText("х");
             turn.setText("o");
+            board[1] =1;
         } else {
             Verh.setText("o");
             turn.setText("x");
+            board[1] =2;
         }
+        Verh.setDisable(true);
+        checkWinner();
     }
 
     @FXML
@@ -50,10 +108,14 @@ public class HelloController {
         if (turn.getText().equals("x")) {
             Rverh.setText("х");
             turn.setText("o");
+            board[2] =1;
         } else {
             Rverh.setText("o");
             turn.setText("x");
+            board[2] =2;
         }
+        Rverh.setDisable(true);
+        checkWinner();
     }
 
     @FXML
@@ -61,10 +123,14 @@ public class HelloController {
         if (turn.getText().equals("x")) {
             Lmid.setText("х");
             turn.setText("o");
+            board[3] =1;
         } else {
             Lmid.setText("o");
             turn.setText("x");
+            board[3] =2;
         }
+        Lmid.setDisable(true);
+        checkWinner();
     }
 
     @FXML
@@ -72,10 +138,14 @@ public class HelloController {
         if (turn.getText().equals("x")) {
             centre.setText("х");
             turn.setText("o");
+            board[4] =1;
         } else {
             centre.setText("o");
             turn.setText("x");
+            board[4] =2;
         }
+        centre.setDisable(true);
+        checkWinner();
     }
 
 
@@ -84,10 +154,14 @@ public class HelloController {
         if (turn.getText().equals("x")) {
             Rmid.setText("х");
             turn.setText("o");
+            board[5] =1;
         } else {
             Rmid.setText("o");
             turn.setText("x");
+            board[5] =2;
         }
+        Rmid.setDisable(true);
+        checkWinner();
     }
 
     @FXML
@@ -95,10 +169,14 @@ public class HelloController {
         if (turn.getText().equals("x")) {
             Ldown.setText("х");
             turn.setText("o");
+            board[6] =1;
         } else {
             Ldown.setText("o");
             turn.setText("x");
+            board[6] =2;
         }
+        Ldown.setDisable(true);
+        checkWinner();
     }
 
     @FXML
@@ -106,10 +184,14 @@ public class HelloController {
         if (turn.getText().equals("x")) {
             Down.setText("х");
             turn.setText("o");
+            board[7] =1;
         } else {
             Down.setText("o");
             turn.setText("x");
+            board[7] =2;
         }
+        Down.setDisable(true);
+        checkWinner();
     }
 
     @FXML
@@ -117,9 +199,13 @@ public class HelloController {
         if (turn.getText().equals("x")) {
             Rdown.setText("х");
             turn.setText("o");
+            board[8] =1;
         } else {
             Rdown.setText("o");
             turn.setText("x");
+            board[8] =2;
         }
+        Rdown.setDisable(true);
+        checkWinner();
     }
 }
